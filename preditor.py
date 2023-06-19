ee100f220d4036bc13161775f8b0c6ec6bd745ba
Philipe Rocha <philipe.bh@gmail.com>
2023-06-19 19:36:52 -0300

    finalização das implementações
    - funcao prevendo_o_futuro que ativa a rede

--- preditor.py
+++ preditor.py
@@ -8,6 +8,10 @@ import pandas as pd
 from pybrain.datasets  import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import LinearLayer, SigmoidLayer, SoftmaxLayer
+
+from pybrain.tools.customxml import NetworkWriter
+from pybrain.tools.customxml import NetworkReader
 
 
 
@@ -18,8 +22,29 @@ print('Carregando os ativos...')
 
 DSnetflix = netflix.history(period='max', interval='1d')
 
+DSnetflix.to_csv('base_final.csv')
+
 # open|volume|high|low|close|mme
-preditor = buildNetwork(6, 8, 8, 8, 8, 8, 1)
+""""
+melhores resultados 
+neuronios x camadas 
+200 x 5 
+200 x 3 => 36
+300 x 3 => 36
+400 x 3 => 32.
+400 x 2 => 34
+"""
+# neuronios_nbr = 200
+neuronios_nbr = 400
+
+print('Montando a rede neural...')
+preditor = buildNetwork(5,
+                        neuronios_nbr, neuronios_nbr, neuronios_nbr,
+                        1)
+
+# preditor = NetworkReader.readFrom('')
+
+print(preditor)
 
 
 def media_movel_exp(periodo, DS):
@@ -73,20 +98,40 @@ def carregar_o_futuro(dias_a_frente, ds):
 def aprendendo_o_futuro(ds, epocas):
     print('Iniciando base de treino...')
 
-    base = SupervisedDataSet(6, 1)
+    base = SupervisedDataSet(5, 1)
 
     for idx, row in ds.iterrows():
-        base.addSample((row['open'], row['volume'],
-                        row['high'], row['low'],
-                        row['close'], row['mme'] ),
-                       (row['target'],))
+        base.addSample((row['open'], row['high'], row['low'],
+                        row['close'], row['mme'] ), (row['target'],))
 
-    treinador = BackpropTrainer(preditor, dataset=base, learningrate=0.01, momentum=0.06)
+    treinador = BackpropTrainer(preditor, dataset=base, learningrate=0.01, momentum=0.06, weightdecay=0.01)
     print('Iniciando treinamento...')
     for i in range(1, epocas):
         erro = treinador.train()
-        if i % 10 == 0:
-            print('Erro: %s' % erro)
+        if i % 1 == 0:
+            print(f"Erro da epoca {i}: {erro}")
+            NetworkWriter.writeToFile(preditor, 'preditor_400x3_final.xml')
+            # treinador.testOnData(dataset=base, verbose=False)
+
+    NetworkWriter.writeToFile(preditor, 'preditor_400x3_final.xml')
+
+
+def prevendo_o_futuro(ds):
+    print('Prevendo o futuro...')
+    preditos = []
+    diffs = []
+    for idx, row in ds.iterrows():
+        predicao = preditor.activate([row['open'], row['high'], row['low'], row['close'], row['mme']])[0]
+        preditos.append(predicao)
+
+        diff = abs( row['target'] - predicao )
+        diffs.append(diff)
+
+    ds.loc[:, 'preditos'] = preditos
+    ds.loc[:, 'diferenca'] = diffs
+    ds.to_csv('preditos_final.csv')
+
+    return ds
 
 
 # A func abaixo faz o calculo da média móvel para o DS
@@ -94,20 +139,22 @@ def aprendendo_o_futuro(ds, epocas):
 DB = media_movel_exp(21, DSnetflix)
 
 # a função abaixo cria uma nova coluna no dataset com o valor de fechamento do ativo para o periodo informado
-DB = carregar_o_futuro(15, DB)
+DB = carregar_o_futuro(2, DB)
 
 
 tamanho_db = DB.__len__()
 
 index_list_bd = DB.index
-limite_treino = round(tamanho_db/2)
+limite_treino = round((tamanho_db/3) * 2)
 
 base_treino = DB.loc[index_list_bd[0]:index_list_bd[limite_treino]]
 
-aprendendo_o_futuro(base_treino, 10000)
 
+aprendendo_o_futuro(base_treino, 1000)
 
+base_teste = DB.loc[index_list_bd[++limite_treino]:index_list_bd[tamanho_db]]
 
+prevendo_o_futuro(base_treino)
 
 # print(DB['target'])
 # DB.to_csv('with_taget.csv')
